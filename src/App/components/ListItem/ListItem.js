766e532f40fcab62179c2193ea093095df2bb8fb
Denis Frolov <d.fr8938@gmail.com>
2025-09-20 09:54:26 +0300

    changed styles

--- src/App/components/ListItem/ListItem.js
+++ src/App/components/ListItem/ListItem.js
@@ -4,7 +4,7 @@ const ListItem = ({ item, i }) => {
   return (
     <li key={item.id} className="list-item">
       <h3 className="list-item-question">
-        {++i + ". "}
+        <b>{++i + ". "}</b>
         {item.question}
       </h3>
       <p className="list-item-answer">{item.answer}</p>
